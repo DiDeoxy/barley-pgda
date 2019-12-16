8a77cac2050950585888d83c367777106e82921d
Max Hargreaves <max_hargreaves@outlook.com>
2019-12-16 00:45:26 +0000

    save point

--- R/ld_pruning.R
+++ R/ld_pruning.R
@@ -1,6 +1,8 @@
+library(igraph)
 library(parallel)
 library(SeqArray)
 library(SNPRelate)
+library(tibble)
 library(magrittr, "%>%")
 import::from(plyr, "llply")
 import::from(readr, "read_tsv", "read_rds", "write_rds")
@@ -14,7 +16,8 @@ var_sets <- as.character(read.gdsn(index.gdsn(barley, "variant.id"))) %>%
 phys_pos <- as.numeric(read.gdsn(index.gdsn(barley, "position"))) %>%
     split(chrom)
 
-size <- 20
+window <- 50
+size <- window * 2 + 1
 
 ld_mats <- lapply(var_sets, function (var_set) {
     snpgdsLDMat(barley, slide = size, snp.id = var_set, num.thread = 8)$LD
@@ -38,10 +41,6 @@ gen_pos <- lapply(ld_mats, function (ld_mat) {
     }, mc.cores = 8) %>% unlist() %>% as.numeric() %>% cumsum()
 })
 
-cbind(
-    c(rep(1, size), 2:(col - (size - 1))), 1:(col)
-)
-
 png(
    "/workspace/barley/results/phys_vs_recip_mean_LD.png",
   family = "Times New Roman", width = 220, height = 1400, pointsize = 5,
@@ -52,6 +51,8 @@ for (i in 1:7) {
     plot(phys_pos[[i]], gen_pos[[i]])
 }
 dev.off()
+
+################################################################################
 
 make_rows <- function(cols, start, size) {
     cols <- c(start:cols[1], cols)
@@ -64,11 +65,13 @@ make_rows <- function(cols, start, size) {
     
 }
 
-densities <- lapply(ld_mats, function (ld_mat) {
+calc_densities <- function (ld_mat, cores) {
+    size <- (nrow(ld_mat) - 1) / 2
     intervals <- cbind(
         c(rep(1, size + 1), 2:(ncol(ld_mat) - (size))),
-        c((size):(ncol(ld_mat)  - 1), rep(ncol(ld_mat) , size))
+        c(size:(ncol(ld_mat) - 1), rep(ncol(ld_mat), size))
     ) %>% split(., seq(nrow(.)))
+    # print(intervals)
     mclapply(intervals, function (interval) {
         rng <- interval[1]:interval[2]
         if (length(rng) < (size * 2) && interval[1] == 1) {
@@ -93,23 +96,145 @@ densities <- lapply(ld_mats, function (ld_mat) {
         } else {
             return(density)
         }
-    }, mc.cores = 8) %>% unlist() %>% as.numeric()
-})
+    }, mc.cores = cores) %>% unlist() %>% as.numeric()
+}
 
-library(igraph)
+calc_densities <- function (ld_mat, cores) {
+    int_size <- min(size, ncol(ld_mat))
+    mclapply(1:ncol(ld_mat), function (i) {
+        if (i == 1) {
+            cols <- 1
+            rows <- 1:int_size
+            rows <- rows[which(rows <= int_size)]
+        } else if (i == ncol(ld_mat)) {
+            cols <- (ncol(ld_mat) - int_size):(ncol(ld_mat) - 1)
+            cols <- cols[which(cols > 0)]
+            rows <- length(cols):1
+        } else if (i <= int_size) {
+            if (ncol == int_size) {
+                cols <- c(1:(i - 1), rep(i, int_size - i))
+                rows <- c((i - 1):1, 1:(int_size - i))
+            } else {
+                cols <- c(1:(i - 1), rep(i, int_size - i + 1))
+                rows <- c((i - 1):1, 1:(int_size - i + 1))
+            }
+        } else if (i >= (ncol(ld_mat) - int_size)) {
+            if ((ncol - int_size) + 1 == i) {
+                cols <- c((i - int_size):(ncol(ld_mat) - int_size), rep(i, ncol(ld_mat)  - i))
+                rows <- c((ncol(ld_mat)  - int_size - 1):1, 1:(ncol(ld_mat)  - i))
+            } else {
+                cols <- c((i - int_size):(ncol(ld_mat)  - int_size + 1), rep(i, ncol(ld_mat)  - i))
+                rows <- c((ncol(ld_mat)  - int_size - 1):1, 1:(ncol(ld_mat)  - i))
+            }
+        } else {
+            cols <- c((i - int_size):i, rep(i, int_size - 1))
+            rows <- c(int_size:1, 1:int_size)
+        }
+        indices <- cbind(rows, cols)
+        lds <- abs(ld_mat[indices])
+        lds[is.nan(lds)] <- NA
+        density <- mean(lds, na.rm = TRUE)
+        if (is.nan(density)) {
+            return(1)
+        } else {
+            return(density)
+        }
+    }, mc.cores = cores) %>% unlist() %>% as.numeric()
+}
+i = 7
+ncol = 13
+int_size = 12
+if (ncol == int_size) {
+    cols <- c(1:(i - 1), rep(i, int_size - i))
+    rows <- c((i - 1):1, 1:(int_size - i))
+} else {
+    cols <- c(1:(i - 1), rep(i, int_size - i + 1))
+    rows <- c((i - 1):1, 1:(int_size - i + 1))
+}
 
-prop = 0.5
-window <- 10
-threshold <- 0.99
-snp_set <- mclapply(1:length(densities), function (i) {
-    chrom <- data.frame(id = var_sets[[i]], density = densities[[i]], pos = gen_pos[[i]])
+cbind(rows, cols)
+
+
+
+densities <- lapply(ld_mats, calc_densities, 8)
+str(var_sets[[i]])
+ld_prune <- function (i) {
+    chrom <- tibble(id = var_sets[[i]], density = densities[[i]])
     n <- (nrow(chrom) * prop) %>% floor()
     while (nrow(chrom) > n) {
-        densest <- chrom$id[(which.max(chrom$density) - window):(which.max(chrom$density) + window)] %>% as.character()
-        comps <- combn(densest, 2)
-        el <- comps[, (abs(snpgdsLDMat(barley, snp.id = densest, num.thread = 8)$LD) > threshold) %>% as.vector() %>% na.omit()] %>% t()
-        largest_cliques(graph_from_edgelist(el, directed = FALSE)) %>% print()
+        # find the current densest marker
+        densest <- which.max(chrom$density)
+        # create a window around the densest marker
+        markers <- (densest - window):(densest + window)
+        # select those snps that actually exist in chrom and extract their ids
+        markers <- chrom$id[which((markers >= 1) & (markers <= nrow(chrom)))]
+        # find all combinations of size 2 of markers in this window
+        comps <- combn(markers, 2)
+        # get the LD mat of these markers, find which ones are in LD greater
+        # than the threshold value, transform it into a vector columnwise and
+        # omit NAs from the vector. This creates a vector with indices that
+        # match the combos at the same indices in comps. Identify which indices
+        # of the vector are true and subset the columns of comps by this and
+        # transpose to create the edge list (which variants are in LD above the
+        # threshold to each other)
+        el <- comps[, which((abs(snpgdsLDMat(barley, snp.id = markers, num.thread = 1)$LD) > threshold) %>% as.vector() %>% na.omit())] %>% t()
+        # use the edgelist to idenify the largest clique in the window
+        clique <- largest_cliques(graph_from_edgelist(el, directed = FALSE))[[1]]$name
+        # identify the left and rightmost markers in the clique, auto transforms
+        # into numeric
+        clique_range <- range(clique)
+        # identify all possible markers in this range
+        covered <- clique_range[1]:clique_range[2] %>% as.character()
+        # identify those markers in this range that are still present in the
+        # chrom data frame
+        present <- which(chrom$id %in% covered)
+        # identify the densest marker in the clique since this may be different
+        # from the densest marker in the window
+        clique_densest <- chrom$id[present][which.max(chrom$density[present])]
+        # remove all but the densest marker in the clique range from chrom
+        chrom <- chrom[-which(chrom$id %in% covered[-which(covered %in% clique_densest)]), ]
+        # identify which row the densest marker now occupies in chrom
+        densest_row <- which(chrom$id %in% clique_densest)
+        # identify the markers whose densities need to be recalcualted due to
+        # the removal of the markers in the clique range
+        minor_range_rows <- (densest_row - window):(densest_row + window)
+        # it is possible for the range to extend beyond the rows of chrom
+        # therfore i remove these from the range
+        minor_range_rows <- minor_range_rows[which((minor_range_rows >= 1) & (minor_range_rows <= nrow(chrom)))]
+        # to calculate densities we must consider markers beyond the range of
+        # those whose density need to be recalculated as the density of a marker
+        # includes those both upstream and downstream up to window away
+        major_range_rows <- (densest_row - window * 2):(densest_row + window * 2)
+        # same issue as with minor_range_rows
+        major_range_rows <- major_range_rows[which((major_range_rows >= 1) & (major_range_rows <= nrow(chrom)))]
+        # get the ids of these markers
+        snpset_ids <- chrom$id[major_range_rows]
+        # calculate the LD of the snpset markers to each other
+        ld <- snpgdsLDMat(barley, slide = size, snp.id = snpset_ids, num.thread = 1, verbose = FALSE)$LD
+        print(dim(ld))
+        # identify the indices of the markers in snpset_id that correspond to
+        # the ones that need to be updated
+        to_change <- which(major_range_rows %in% minor_range_rows)
+        # calculate the densities of these markers and update them
+        chrom$density[major_range_rows[to_change]] <- calc_densities(ld, 1)[to_change]
+        print((nrow(chrom) / length(var_sets[[i]])))
     }
-}, mc.cores = 1)
+    chrom
+}
 
-combn(1:3, 2)
+prop <- 0.2
+threshold <- 0.7
+snp_sets <- mclapply(1:length(densities), ld_prune, mc.cores = 1)
+
+which(! var_sets[[1]] %in% snp_sets[[1]]$id)
+################################################################################
+png(
+   "/workspace/barley/results/phys_vs_recip_mean_LD_pruned.png",
+  family = "Times New Roman", width = 220, height = 1400, pointsize = 5,
+  units = "mm", res = 192
+)
+par(mfrow = c(7, 1))
+for (i in 1:7) {
+    plot(phys_pos[[i]][var_sets[[i]] %in% snp_sets[[i]]$id], gen_pos[[i]][var_sets[[i]] %in% snp_sets[[i]]$id])
+}
+dev.off()
